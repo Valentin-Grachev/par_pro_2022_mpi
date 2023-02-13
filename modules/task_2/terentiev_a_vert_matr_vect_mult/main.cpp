276b92fde1164be7ee9e1b6e379ac9d2142f656c
Valentin-Grachev <v.alentin52.v@yandex.ru>
2023-02-13 18:51:34 +0300

    Test4

--- modules/task_2/terentiev_a_vert_matr_vect_mult/main.cpp
+++ modules/task_2/terentiev_a_vert_matr_vect_mult/main.cpp
@@ -4,7 +4,7 @@
 #include <gtest-mpi-listener.hpp>
 
 
-void Test(int size) {
+void TestMultiple(int size) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
@@ -22,15 +22,15 @@ void Test(int size) {
     }
 }
 
-TEST(Matr_Vect_Mult_Mpi, 5x5) { Test(5); }
+TEST(Matr_Vect_Mult_Mpi, Test_5x5) { TestMultiple(5); }
 
-TEST(Matr_Vect_Mult_Mpi, 10x10) { Test(10); }
+TEST(Matr_Vect_Mult_Mpi, Test_10x10) { TestMultiple(10); }
 
-TEST(Matr_Vect_Mult_Mpi, 50x50) { Test(50); }
+TEST(Matr_Vect_Mult_Mpi, Test_50x50) { TestMultiple(50); }
 
-TEST(Matr_Vect_Mult_Mpi, 100x100) { Test(100); }
+TEST(Matr_Vect_Mult_Mpi, Test_100x100) { TestMultiple(100); }
 
-TEST(Matr_Vect_Mult_Mpi, 200x200) { Test(200); }
+TEST(Matr_Vect_Mult_Mpi, Test_200x200) { TestMultiple(200); }
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
